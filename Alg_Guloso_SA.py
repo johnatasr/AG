d209757aee996766e4bcd5bf282e6dbcdcdccf9f
johnatasr <johnatasr@hotmail.com>
2019-06-05 19:41:36 -0300

    2o commit do dia  :)

--- Alg_Guloso_SA.py
+++ Alg_Guloso_SA.py
@@ -13,8 +13,6 @@ def MaxAtividades(s, f ):
     i = 0
     
     
-    
-
     # Considere o resto das atividades
     for j in range(n): 
   
@@ -24,16 +22,12 @@ def MaxAtividades(s, f ):
         if s[j] >= f[i] : 
             print(j) 
             i = j
-            if s[0] == 1:
-                print(i)   
 
     # Testes
 # s = [1, 3, 0, 5, 8, 5]
 # f = [2, 4, 6, 7, 9, 9]
 
-s = [4, 1, 5, 5, 8]
-f = [5, 5, 6, 7, 9]
+s = [3, 5, 1, 5, 7]
+f = [4, 6, 6, 9, 9]
 
 MaxAtividades(s, f)
-  
-
--- PD_Top_Down.py
+++ PD_Top_Down.py
@@ -1,3 +1,5 @@
+#top-down
+
 INF = 100000;
 r = [0] + [-1*INF]*5
 
@@ -6,7 +8,7 @@ def max(x, y):
     return x
   return y
 
-def top_down_rod_cutting(c, n):
+def corta_haste(c, n):
   global r
   if(r[n] >= 0):
     return r[n]
@@ -14,14 +16,14 @@ def top_down_rod_cutting(c, n):
   max_valor = -1*INF
 
   for i in range(1, n+1):
-    max_valor = max(max_valor, c[i] + top_down_rod_cutting(c, n-i))
+    max_valor = max(max_valor, c[i] + corta_haste(c, n-i))
 
   r[n] = max_valor
   return r[n]
 
 if __name__ == '__main__':
-  # list starting from 1, element at index 0 is fake
-  c = [0, 1, 2, 3, 5]
+
+  c = [0, 1, 15, 3, 5]
   frase = "A melhor escolha é: "
   print(frase)
-  print(top_down_rod_cutting(c, 4))
+  print(corta_haste(c, 4))
